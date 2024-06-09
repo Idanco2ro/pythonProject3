4445a6b5bc5046c371d3aeddf3b4154c0aeea1a9
Daniel Ivanov <i_danco@yahoo.com>
2024-06-09 22:20:27 +0300

    String , Variables practice

---
+++ Day1.py
@@ -0,0 +1,50 @@
+"""
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+print("Hello world\nHello world\nHello world")
+
+print("Hello" + ' ' + "Angela" + "!")
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print('e.g. print("Hello"+"World")')
+print("New line can be created with a backslash and n.")
+"""
+"""
+input("What is your name?")
+
+# input() vom scrie numele in consola
+# print() va afisa Hello + ce am scris in consola (numele) + !
+print("Hello " + input("What is your name?") + "!")
+
+print( len( input("What is your name? ") ) )
+
+#
+name=input("What is your name? ")
+print(name)
+length=len(name)
+print(length)
+"""
+"""
+# Day1.4 Variables Exercise
+a = input("a: ")
+b = input("b: ")
+c = a
+a = b
+b = c
+print("a=" + a)
+print("b=" + b)
+"""
+"""
+print("Hello !")
+name_of_the_city = input("Type your city you grew up:\n ")
+
+name_of_the_pet = input("Type the name of your pet:\n ")
+
+
+print("Your band name could be:\n" + name_of_the_city + " " + name_of_the_pet)
+"""
+
+
